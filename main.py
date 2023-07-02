c2ed39fbc9de54504139b2858ee76dcd1715063a
Jetonic <531277498@qq.com>
2023-07-02 08:46:17 +0800

    开始

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup/')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/rank')
+def rank():
+    return render_template('rank.html')
+
+@app.route('/money')
+def money():
+    return render_template('money.html')
+
+@app.route('/personal/<id>')
+def personal(id):
+    return render_template('personal.html',id=id)
+
+@app.route('/favorite/<id>')
+def favorite(id):
+    return render_template('favorite.html',id=id)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',debug = True)
