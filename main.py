548a1fa160eff790bfc03677ec47fc19043ae7cf
Mingshu LHYF <531277498@qq.com>
2023-07-03 08:56:45 +0800

    230703 修改

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,session
 import pymysql
 app = Flask(__name__)
-
-loggedid=-1
+app.secret_key='your_secret_key'
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'id' in session:
+        return render_template("index.html",loggedid=session['id'])
+    return render_template('index.html',loggedid=-1)
 
 @app.route('/login/',methods=['GET', 'POST'])
 def login():
+    global loggedid
     if request.method=="POST":
         username=request.form['nm']
         password=request.form['pw']
@@ -18,6 +20,8 @@ def login():
         user=cursor.fetchone()
         if user:
             # 用户登录成功
+            session['id']=user[0]
+            session['username']=user[1]
             return redirect(url_for('index'))
         else:
             error = '无效的用户名或密码，请重试。'
@@ -28,6 +32,33 @@ def login():
 
 @app.route('/signup/')
 def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+        db = pymysql.connect(host="localhost", user="root", password="Jtnic027", database="jobdb")
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM users WHERE name=%s",(username))
+        namelist=cursor.fetchall()  ##判断用户名
+        cursor.execute("SELECT * FROM users WHERE email=%s",(email))
+        emaillist=cursor.fetchall() ##判断邮箱
+        if(len(namelist) or len(emaillist)):
+            error="用户名或邮箱已经注册"
+            return render_template('signup.html',error=error)
+        elif(password2!=password):
+            error="两次密码不一致"
+            return render_template('signup.html', error=error)
+        else:
+            sql="""INSERT INTO users
+            (name,pass,email)
+            VALUES(%s,%s,%s)"""
+            try:
+                cursor.execute(sql,(username,password,email))
+                db.commit()
+            except:
+                db.rollback()
+        db.close()
     return render_template('signup.html')
 
 @app.route('/forget/')
@@ -44,8 +75,9 @@ def money():
 
 @app.route('/personal/<id>')
 def personal(id):
-    global loggedid
-    return render_template('personal.html',id=id,loggedid=loggedid)
+    if 'id' in session:
+        return render_template('personal.html', id=id, loggedid=session['id'])
+    return render_template('personal.html',id=id,loggedid=-1)
 
 @app.route('/movieinfo/<movieid>')
 def movieinfo(movieid):
